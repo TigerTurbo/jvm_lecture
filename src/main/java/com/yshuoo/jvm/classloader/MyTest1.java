11c955162942cfa7a5bc45459a2e54dce1ddeff0
shuo.yang <shuo.yang@Ctrip.com>
2019-11-05 20:32:12 +0800

    初始化

---
+++ src/main/java/com/yshuoo/jvm/classloader/MyTest1.java
@@ -0,0 +1,19 @@
+package com.yshuoo.jvm.classloader;
+
+/**
+ * @author yangshuo
+ * @date 2019/11/5 20:30
+ */
+public class MyTest1 {
+
+    public static void main(String[] args){
+
+    }
+
+}
+
+class MyParent{
+    static{
+        System.out.println("MyParent");
+    }
+}
